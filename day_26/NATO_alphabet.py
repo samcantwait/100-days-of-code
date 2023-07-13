ea31caa11d2bb7f66c0e03a860a7e7fbd5e1926f
Samantha Atkins <sam@samcantwait.com>
2023-07-13 08:24:18 -0500

    redo the NATO alphabet challenge

--- day_26/NATO_alphabet.py
+++ day_26/NATO_alphabet.py
@@ -1,8 +1,8 @@
 import pandas
 
-data = pandas.read_csv('nato_phonetic_alphabet.csv')
+alphabet = pandas.read_csv("./nato_phonetic_alphabet.csv")
+dictionary = {row.letter: row.code for (index, row) in alphabet.iterrows()}
 
-NATO_dict = {row.letter:row.code for (index, row) in data.iterrows()}
-
-word = input('Enter your word: ').upper()
-output_list = [NATO_dict[letter] for letter in word]
+user_word = input("Enter your word: ").upper()
+in_code = [dictionary[letter] for letter in user_word]
+print(in_code)
